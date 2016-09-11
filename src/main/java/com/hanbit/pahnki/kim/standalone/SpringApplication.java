0270e7359866918fbe4c7b1b12bed55c970d5a79
hanbit <hanbit@hanbit-java001>
2016-09-11 22:51:34 +0900

    최초 커밋

---
+++ src/main/java/com/hanbit/pahnki/kim/standalone/SpringApplication.java
@@ -0,0 +1,23 @@
+package com.hanbit.pahnki.kim.standalone;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.Log4jConfigurer;
+
+public class SpringApplication {
+
+	public static void main(String[] args) {
+		Logger LOGGER = LoggerFactory.getLogger(SpringApplication.class);
+
+		try {
+			Log4jConfigurer.initLogging("classpath:config/log4j.xml");
+
+			ApplicationContext context = new ClassPathXmlApplicationContext("spring/applicationContext-core.xml");
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage(), e);
+		}
+	}
+
+}
